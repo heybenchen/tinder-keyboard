6cdff722f2098a2d9c98140204c51c1a12cb89f6
Ben Shuyi Chen <benshuyichen@gmail.com>
2016-05-15 14:17:28 -0700

    Assigning random typefaces to letters

--- app/src/main/java/stupidhackathon/tinderkeyboard/Card.java
+++ app/src/main/java/stupidhackathon/tinderkeyboard/Card.java
@@ -1,24 +1,44 @@
 package stupidhackathon.tinderkeyboard;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.Random;
+
 public class Card {
 
     private Context mContext;
     private String mLetter;
+    private Typeface mTypeface;
+
+    private String[] mFontNames;
 
     public Card(Context context, String letter) {
+        final Random random = new Random();
+
         mContext = context;
         mLetter = letter;
+
+        try {
+            mFontNames = context.getAssets().list("fonts");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final String fontName = "fonts/" +  mFontNames[random.nextInt(mFontNames.length)];
+        mTypeface = Typeface.createFromAsset(mContext.getAssets(), fontName);
     }
 
     public View getLetterView(ViewGroup parentView) {
-        TextView textView = (TextView) LayoutInflater.from(mContext).inflate(R.layout.letter_simple, parentView, false);
+        TextView textView = (TextView) LayoutInflater.from(mContext)
+                .inflate(R.layout.letter_simple, parentView, false);
         textView.setText(mLetter);
+        textView.setTypeface(mTypeface);
         return textView;
     }
 }
